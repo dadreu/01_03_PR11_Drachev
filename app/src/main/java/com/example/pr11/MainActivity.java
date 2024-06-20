a1e919b2a231b1af3c911dbba494e2ac70fa863f
dadreu <dadreu@gmail.com>
2024-06-20 16:00:20 +0500

    Выполненная работа. Драчёв

---
+++ app/src/main/java/com/example/pr11/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.pr11;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void AlertDialog(String title, String message)
+    {
+     AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+     builder.setTitle(title)
+             .setMessage(message)
+             .setCancelable(false)
+             .setNegativeButton("OK",
+                     new DialogInterface.OnClickListener() {
+                         @Override
+                         public void onClick(DialogInterface dialog, int which) {
+                             dialog.cancel();
+                         }
+                     });
+     AlertDialog alert = builder.create();
+     alert.show();
+    }
+    public void OnClick(View view){
+        AlertDialog("Уведомление", "Нажатие обработано");
+    }
+
+}
